1be38c852fc5605fd51a76a6fbc046fed14c21b0
NisemonoQ <yann.badinga@gmail.com>
2024-10-08 21:50:08 +0200

    Caméra ver.1 sans cinémachine

--- Assets/Scripts/Caméra/CamMov.cs
+++ Assets/Scripts/Caméra/CamMov.cs
@@ -2,22 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Camera))]
 public class CamMov : MonoBehaviour
 {
-    public Transform target;
+    [Header("Notre magnifique Caméra")]
+    private Camera cam; 
+
+    [Header("Transform des personnages")]
+    public List<Transform> targets;
+
+    [Header("Des Vecteurs")]
     public Vector3 offset;
-    public float damping;
+    private Vector3 velocity;
 
-    Vector3 velocity = Vector3.zero;
+    [Header("Variables Nuémriques")]
+    public float smoothFactor = .8f;
+    public float minZoom = 40f;
+    public float maxZoom = 10f;
+    public float zoomLimit = 50f;
 
-    private void Awake()
+    void Start()
     {
-        offset = transform.position - target.position;
+        cam = GetComponent<Camera>(); 
     }
 
-    void Update()
+    private void LateUpdate()
     {
-        Vector3 movPos = target.position + offset;
-        transform.position = Vector3.SmoothDamp(transform.position, movPos, ref velocity, damping);
+        if (targets.Count == 0)
+            return;
+
+        MoveCam();
+
+        Zoomin(); 
+
+    }
+
+    void Zoomin()
+    {
+        //Debug.Log(GetGreatestDistance());
+        float newZoom = Mathf.Lerp(minZoom, maxZoom, GetGreatestDistance()/zoomLimit);
+        cam.fieldOfView = Mathf.Lerp(cam.fieldOfView, newZoom, Time.deltaTime); 
+    }
+    
+    void MoveCam()
+    {
+        Vector3 centerPos = GetCenter();
+        Vector3 newPos = centerPos + offset;
+
+        transform.position = Vector3.SmoothDamp(transform.position, newPos, ref velocity, smoothFactor);
+    }
+    float GetGreatestDistance()
+    {
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.size.y;
+    }
+
+    Vector3 GetCenter()
+    {
+        if(targets.Count == 1)
+        {
+            return targets[0].position;
+        }
+
+        var bounds = new Bounds(targets[0].position, Vector3.zero);
+        for(int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+
+        return bounds.center; 
     }
 }
